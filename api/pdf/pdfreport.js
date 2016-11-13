5830ab31ad33a858d48b9044adf22db176f45320
Morten Rasmussen <cyrixmorten@MacBook-Pro-4.local>
2016-11-13 22:37:54 +0100

    Send buffer in report REST api

--- api/pdf/pdfreport.js
+++ api/pdf/pdfreport.js
@@ -3,8 +3,10 @@ var reportToPdf = require('../../cloud/pdf/reportToPDF')
 module.exports = function (req, res) {
 
     reportToPdf.toPdf(req.params.id).then(function(response) {
-        res.set('Content-Type: application/octet-stream');
-        res.status(200).send(response);
+        // res.set('Content-Type: application/octet-stream');
+
+        res.status(200);
+        res.send(response.buffer);
     }).fail(function(error) {
         res.status(400).send(error);
     });
