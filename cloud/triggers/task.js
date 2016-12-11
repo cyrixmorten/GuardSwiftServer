736c6e7f2f32f3c15dc3e2d789d510c47cc10066
Morten Rasmussen <cyrixmorten@gmail.com>
2016-12-11 19:18:41 +0100

    Send SMS to guards on incoming alarm

--- cloud/triggers/task.js
+++ cloud/triggers/task.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var cpsms = require('../../api/cpsms');
 
 var handlers = require('../centrals/all');
 
@@ -52,17 +53,36 @@ exports.reset = function(task) {
 
 var sendNotification = function(alarm) {
 
-    var query = new Parse.Query(Parse.Installation);
-    query.equalTo('owner', alarm.get('owner'));
-    query.equalTo('channels', 'alarm');
+    var sendPushNotification = function() {
+        var installationQuery = new Parse.Query(Parse.Installation);
+        installationQuery.equalTo('owner', alarm.get('owner'));
+        installationQuery.equalTo('channels', 'alarm');
 
-    Parse.Push.send({
-        where: query,
-        expiration_time: 600000,
-        data: {
-            alarmId: alarm.id
-        }
-    }, { useMasterKey: true });
+        Parse.Push.send({
+            where: installationQuery,
+            expiration_time: 600000,
+            data: {
+                alarmId: alarm.id
+            }
+        }, { useMasterKey: true });
+    };
+
+    var sendSMS = function () {
+        var guardQuery = new Parse.Query("Guard");
+        guardQuery.equalTo('owner', alarm.get('owner'));
+        guardQuery.equalTo('alarmNotify', true);
+        guardQuery.equalTo('alarmSMS', true);
+        guardQuery.exists('mobileNumber', true);
+        guardQuery.each(function (guard) {
+            cpsms.send({
+                to: guard.get("mobileNumber"),
+                message: alarm.get("original")
+            });
+        }, { useMasterKey: true });
+    };
+
+    sendPushNotification();
+    sendSMS();
 };
 
 var alarmUpdate = function(task) {
