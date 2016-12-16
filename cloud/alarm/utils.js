6b34a96dda4f2fbcb67a89d8c1544e50aa07327a
Morten Rasmussen <cyrixmorten@gmail.com>
2016-12-16 23:03:31 +0100

    Debug options

--- cloud/alarm/utils.js
+++ cloud/alarm/utils.js
@@ -1,7 +1,9 @@
 var _ = require('lodash');
 var geocode = require('../utils/geocode.js');
 
-exports.findAlarm = function(options) {
+exports.findAlarm = function (options) {
+    console.log('options: ', options);
+
     var Alarm = Parse.Object.extend('Task');
     var query = new Parse.Query(Alarm);
     query.equalTo('central', options.central);
@@ -11,26 +13,26 @@ exports.findAlarm = function(options) {
     return query.first({useMasterKey: true});
 };
 
-exports.findCentral = function(sender) {
+exports.findCentral = function (sender) {
     console.log('findCentral');
 
     var Central = Parse.Object.extend('Central');
     var query = new Parse.Query(Central);
     query.equalTo('sendFrom', sender);
 
-    return query.first({ useMasterKey: true });
+    return query.first({useMasterKey: true});
 };
 
-exports.findUser = function(receiver) {
+exports.findUser = function (receiver) {
     console.log('findUser');
 
     var query = new Parse.Query(Parse.User);
     query.equalTo('sendTo', receiver);
 
-    return query.first({ useMasterKey: true });
+    return query.first({useMasterKey: true});
 };
 
-exports.findClient = function(user, fullAddress) {
+exports.findClient = function (user, fullAddress) {
     console.log('findClient', user, fullAddress);
 
     var Client = Parse.Object.extend("Client");
@@ -38,17 +40,17 @@ exports.findClient = function(user, fullAddress) {
     query.equalTo('owner', user);
     query.equalTo('fullAddress', fullAddress);
 
-    return query.first({ useMasterKey: true });
+    return query.first({useMasterKey: true});
 };
 
-exports.createClient = function(user, alarm) {
+exports.createClient = function (user, alarm) {
     console.log('createClient');
 
     var name = alarm.get('clientName');
     var fullAddress = alarm.get('fullAddress');
 
 
-    return geocode.lookupPlaceObject(fullAddress).then(function(placeObject) {
+    return geocode.lookupPlaceObject(fullAddress).then(function (placeObject) {
 
         console.log('placeObject: ' + JSON.stringify(placeObject));
 
@@ -63,7 +65,7 @@ exports.createClient = function(user, alarm) {
         var acl = new Parse.ACL(user);
         client.setACL(acl);
 
-        _.forOwn(placeObject, function(value, key) {
+        _.forOwn(placeObject, function (value, key) {
             client.set(key, value);
         });
 
