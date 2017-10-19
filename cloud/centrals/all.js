105e7f9be175a1b49d32a239e3be079a75d097b2
Morten Rasmussen <cyrixmorten@gmail.com>
2017-10-19 13:13:07 +0200

    Remove javascript and map files from cloud folder

--- cloud/centrals/all.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const g4s = require("./g4s");
-const guardSwift = require("./guardswift");
-exports.centrals = [
-    g4s,
-    guardSwift
-];
-//# sourceMappingURL=all.js.map
