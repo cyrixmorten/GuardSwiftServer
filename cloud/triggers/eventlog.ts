a6cae73b11806204178c4f2362ffd4dc5fda8886
Morten Rasmussen <cyrixmorten@gmail.com>
2018-11-27 22:54:17 +0100

    When appending eventlog to report not use single taskgroupStarted creation time but take all possible groups into account

--- cloud/triggers/eventlog.ts
+++ cloud/triggers/eventlog.ts
@@ -1,6 +1,9 @@
 import {EventLog, TaskEvent} from "../../shared/subclass/EventLog";
 import {Report, ReportQuery} from "../../shared/subclass/Report";
-import {Task, TaskType} from "../../shared/subclass/Task";
+import {Task, TaskQuery, TaskType} from "../../shared/subclass/Task";
+import * as moment from 'moment';
+import * as _ from 'lodash';
+import {TaskGroupStarted} from '../../shared/subclass/TaskGroupStarted';
 
 Parse.Cloud.beforeSave(EventLog, (request, response) => {
 
@@ -31,15 +34,34 @@ let writeEventToReport = async (eventLog: EventLog) => {
 
         let reportQuery = new ReportQuery().matchingClient(eventLog.client);
 
-        if (eventLog.taskGroupStarted) {
-
-            let taskGroupStarted = await eventLog.taskGroupStarted.fetch({useMasterKey: true});
-
-            reportQuery.createdAfterObject(taskGroupStarted);
-        }
-
         if (eventLog.matchingTaskType(TaskType.STATIC, TaskType.ALARM)) {
+            // Simply write one report per task
             reportQuery.matchingTask(task);
+        } else {
+            // Append all task events to the same report
+
+            // 1) Locate all tasks assigned to this client
+            const tasks: Task[] = await new TaskQuery()
+                .matchingClient(eventLog.client)
+                .build()
+                .find({useMasterKey: true});
+
+            // 2) A unique list of all started task groups
+            const taskGroupStartedPointers = _.uniq(_.map(tasks, task => {
+                return task.taskGroupStarted;
+            }));
+
+            // 3) Figure out which of the task groups started the earliest
+            let taskGroupsStarted = await Parse.Object.fetchAll(taskGroupStartedPointers, {useMasterKey: true});
+
+            taskGroupsStarted = _.sortBy<TaskGroupStarted>(taskGroupsStarted, (taskGroupStarted) => taskGroupStarted.timeStarted);
+
+            const firstTaskGroupStarted = _.first(taskGroupsStarted);
+
+            console.log('Earliest possible time', moment(firstTaskGroupStarted.timeStarted).format('DD-MM HH:mm'));
+
+            // 4) Look for existing report created after the earliest possible time
+            reportQuery.createdAfterObject(firstTaskGroupStarted);
         }
 
         return reportQuery
@@ -49,13 +71,13 @@ let writeEventToReport = async (eventLog: EventLog) => {
 
     let writeEvent = async (report: Report, eventLog: EventLog) => {
         console.log('Writing event to report: ' + report.id);
-        console.log('At client:  ' + report.get('clientAddress'));
+        console.log('At client:  ' + report.clientAddress);
 
-        if (eventLog.get('eventCode') === 105) {
-            report.set('extraTimeSpent', eventLog.get('amount'));
+        if (eventLog.eventCode === 105) {
+            report.set('extraTimeSpent', eventLog.amount);
         }
 
-        eventLog.set('reported', true);
+        eventLog.reported = true;
 
         report.addUnique(Report._eventLogs, eventLog);
 
@@ -74,7 +96,7 @@ let writeEventToReport = async (eventLog: EventLog) => {
             }
         }
 
-        report.increment('eventCount');
+        report.incrementEventCount();
 
         return report.save(null, {useMasterKey: true});
     };
@@ -88,7 +110,7 @@ let writeEventToReport = async (eventLog: EventLog) => {
 
     let task: Task = eventLog.task;
 
-    if (task && !eventLog.get('reported')) {
+    if (task && !eventLog.reported) {
         try {
             let report: Report = await findReport(eventLog);
 
