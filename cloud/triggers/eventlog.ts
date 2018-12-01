a16bc6a16d07978fa88bdcc78cc706aee3d0c729
Morten Rasmussen <cyrixmorten@gmail.com>
2018-12-01 22:45:55 +0100

    Improved findReport to filter away task groups that are not being run today

--- cloud/triggers/eventlog.ts
+++ cloud/triggers/eventlog.ts
@@ -4,6 +4,9 @@ import {Task, TaskQuery, TaskType} from "../../shared/subclass/Task";
 import * as moment from 'moment';
 import * as _ from 'lodash';
 import {TaskGroupStarted} from '../../shared/subclass/TaskGroupStarted';
+import {QueryBuilder} from '../../shared/QueryBuilder';
+import {BaseClass} from '../../shared/subclass/BaseClass';
+import {TaskGroup} from '../../shared/subclass/TaskGroup';
 
 Parse.Cloud.beforeSave(EventLog, (request, response) => {
 
@@ -46,21 +49,41 @@ let writeEventToReport = async (eventLog: EventLog) => {
                 .build()
                 .find({useMasterKey: true});
 
-            // 2) A unique list of all started task groups
-            const taskGroupStartedPointers = _.uniq(_.map(tasks, task => {
+            // 2) A unique list of all started task groups matching client tasks
+            const taskGroupStartedPointers = _.compact(_.uniq(_.map(tasks, task => {
                 return task.taskGroupStarted;
-            }));
+            })));
+            const taskGroupsStarted: TaskGroupStarted[] = await Parse.Object.fetchAll(taskGroupStartedPointers, {useMasterKey: true});
+            console.log('taskGroupsStarted', _.map(taskGroupsStarted, TaskGroupStarted._name));
 
-            // 3) Figure out which of the task groups started the earliest
-            let taskGroupsStarted = await Parse.Object.fetchAll(taskGroupStartedPointers, {useMasterKey: true});
+            // 3) Investigate if any task groups are not run today
+            const taskGroupPointers = _.map(taskGroupsStarted, TaskGroupStarted._taskGroup);
+            const taskGroups: TaskGroup[] = await Parse.Object.fetchAll(taskGroupPointers, {useMasterKey: true});
 
-            taskGroupsStarted = _.sortBy<TaskGroupStarted>(taskGroupsStarted, (taskGroupStarted) => taskGroupStarted.timeStarted);
+            const taskGroupsRunToday = _.filter<TaskGroup>(taskGroups, (taskGroup) => taskGroup.isRunToday());
+            console.log('taskGroupsRunToday', _.map(taskGroupsRunToday, TaskGroup._name));
 
-            const firstTaskGroupStarted = _.first(taskGroupsStarted);
+            // 4) Only take those started task groups that run today into account
+            const taskGroupsStartedRunToday = _.filter<TaskGroupStarted>(taskGroupsStarted, (taskGroupStarted: TaskGroupStarted) => {
+                return _.some(taskGroupsRunToday, taskGroup => taskGroup.id === taskGroupStarted.taskGroup.id);
+            });
+            console.log('taskGroupsStartedRunToday', _.map(taskGroupsStartedRunToday, TaskGroupStarted._name));
 
-            console.log('Earliest possible time', moment(firstTaskGroupStarted.timeStarted).format('DD-MM HH:mm'));
+            // 5) Select the task group started that was created the earliest
+            const firstTaskGroupStarted = _.first(
+                _.sortBy<TaskGroupStarted>(taskGroupsStarted, (taskGroupStarted) => taskGroupStarted.timeStarted)
+            );
 
-            // 4) Look for existing report created after the earliest possible time
+            /*
+             * Compare to how it used to be
+             * TODO: Remove
+             */
+            let taskGroupStarted = await eventLog.taskGroupStarted.fetch({useMasterKey: true});
+            console.log('Previous search time', _.pick(taskGroupStarted, TaskGroupStarted._name), moment(taskGroupStarted.createdAt).format('DD-MM HH:mm'));
+            console.log('Current search time', _.pick(firstTaskGroupStarted, TaskGroupStarted._name), moment(firstTaskGroupStarted.timeStarted).format('DD-MM HH:mm'));
+            // TODO: Remove
+
+            // 6) Look for existing report created after the first possible task group started
             reportQuery.createdAfterObject(firstTaskGroupStarted);
         }
 
@@ -71,7 +94,7 @@ let writeEventToReport = async (eventLog: EventLog) => {
 
     let writeEvent = async (report: Report, eventLog: EventLog) => {
         console.log('Writing event to report: ' + report.id);
-        console.log('At client:  ' + report.clientAddress);
+        console.log('At client:  ' + report.clientFullAddress);
 
         if (eventLog.eventCode === 105) {
             report.set('extraTimeSpent', eventLog.amount);
@@ -111,27 +134,32 @@ let writeEventToReport = async (eventLog: EventLog) => {
     let task: Task = eventLog.task;
 
     if (task && !eventLog.reported) {
+        let report: Report;
         try {
-            let report: Report = await findReport(eventLog);
-
-            if (report) {
-                console.log('Found report: ' + report.id);
-                await writeEvent(report, eventLog);
-            }
-            else {
-                try {
-                    console.log('createReport');
-                    await createReport(eventLog);
-                } catch (e) {
-                    console.error('Error while creating report: ' + JSON.stringify(e));
-                }
-            }
-
+            report = await findReport(eventLog);
         } catch (e) {
-            console.error('Unhandled error: ' + JSON.stringify(e));
+            console.error('Error while finding report:');
+            console.error(e);
         }
-    }
-    else if (task) {
+
+        if (report) {
+            try {
+                await writeEvent(report, eventLog);
+            } catch (e) {
+                console.error('Error while writing event to report:');
+                console.error(e);
+            }
+        } else {
+            try {
+                await createReport(eventLog);
+            } catch (e) {
+                console.error('Error while creating report:');
+                console.error(e);
+            }
+        }
+
+
+    } else if (task) {
         console.log('Already written to report');
     } else {
         console.log('Not a report event');
