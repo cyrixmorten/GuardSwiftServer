5748cadc8b1efdbb7977a0b19d3ef162acde4852
Morten Rasmussen <cyrixmorten@gmail.com>
2019-01-01 17:30:59 +0100

    Fix holiday support

--- shared/Planning.ts
+++ shared/Planning.ts
@@ -3,12 +3,12 @@ import * as _ from 'lodash';
 
 export class Planning {
 
-    public static isRunToday(plannedDays: number[], countryCode?: string): boolean {
+    public static isRunToday(plannedDays: number[], countryCode?: string, debug?: boolean): boolean {
         const dayOfWeek = new Date().getDay();
         const isHoliday = new Holidays(countryCode).isHoliday();
 
         const matchPlannedDay = _.includes(plannedDays, dayOfWeek);
-        const matchHoliday = dayOfWeek === 0 && !!isHoliday; // 0 is sunday
+        const matchHoliday = _.includes(plannedDays, 0) && !!isHoliday; // 0 is sunday
 
         return matchPlannedDay || matchHoliday;
     }
