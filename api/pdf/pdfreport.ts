d4a0244496d70100a2f71fdabc54bef45725ddc2
cyrixmorten <cyrixmorten@gmail.com>
2019-08-15 19:18:19 +0200

    Non-customer facing reports now include description of why rows are excluded (#35)

--- api/pdf/pdfreport.ts
+++ api/pdf/pdfreport.ts
@@ -2,7 +2,7 @@ import {ReportToPDF} from "../../cloud/pdf/report.to.pdf";
 
 export let toPdf =  (req, res) => {
 
-    ReportToPDF.buildPdf(req.params.id).then((pdfBuffer: Buffer) => {
+    ReportToPDF.buildPdf(req.params.id, !!req.params.customerFacing).then((pdfBuffer: Buffer) => {
         res.status(200);
         res.send(pdfBuffer);
     }, (error) => {
---
+++ cloud/jobs/send.taskgroup.statistics.ts
@@ -0,0 +1,46 @@
+import * as _ from 'lodash';
+import { TaskType } from '../../shared/subclass/Task';
+import { ReportSettings, ReportSettingsQuery } from '../../shared/subclass/ReportSettings';
+import { Report, ReportQuery } from '../../shared/subclass/Report';
+import { Client } from '../../shared/subclass/Client';
+import { ClientContact } from '../../shared/subclass/ClientContact';
+import { ReportToPDF } from '../pdf/report.to.pdf';
+import { EmailData } from "@sendgrid/helpers/classes/email-address";
+import { AttachmentData } from '@sendgrid/helpers/classes/attachment';
+import { MailData } from '@sendgrid/helpers/classes/mail';
+import { User, UserQuery } from '../../shared/subclass/User';
+import { ReportHelper } from '../utils/ReportHelper';
+import sgMail = require("@sendgrid/mail");
+import moment = require('moment');
+import { Dictionary } from 'lodash';
+import { TaskGroupStarted, TaskGroupStartedQuery } from '../../shared/subclass/TaskGroupStarted';
+import { TaskgroupStartedData as TaskGroupStartedData } from '../statistics/taskgroup.started.data';
+
+export class SendTaskgroupStartedStatistics {
+
+    constructor(private fromDate: Date, private toDate: Date) {}
+
+    async sendToAllUsers(owner: User): Promise<void> {
+
+        new UserQuery().isActive().build().each(async () => {
+
+            const taskGroupsStarted: TaskGroupStarted[] = 
+            await new TaskGroupStartedQuery()
+                .matchingOwner(owner)
+                .createdAfter(this.fromDate)
+                .createdBefore(this.toDate)
+                .build()
+                .find({useMasterKey: true});
+
+            const taskGroupsStartedData: TaskGroupStartedData[] =
+                await Promise.all(_.map(taskGroupsStarted, (taskGroupStarted) => {
+                    return new TaskGroupStartedData(taskGroupStarted).fetch()
+                }))
+
+        }, {useMasterKey: true});
+
+
+    }
+
+ 
+}
