fdfacd4c4bfd41ab2f695ff72607568a44417619
Morten Rasmussen <cyrixmorten@gmail.com>
2021-08-08 15:07:43 +0200

    fix: exclude identical report entries

--- cloud/pdf/excluders/exclude.identical.strategy.ts
+++ cloud/pdf/excluders/exclude.identical.strategy.ts
@@ -1,5 +1,5 @@
-import { EXCLUDE_MODE, ExcludeStrategy } from './exclude.strategy';
-import { EventLog, TaskEvent } from '../../../shared/subclass/EventLog';
+import {EXCLUDE_MODE, ExcludeStrategy} from './exclude.strategy';
+import {EventLog, TaskEvent} from '../../../shared/subclass/EventLog';
 import _ = require('lodash');
 
 export class ExcludeIdenticalStrategy extends ExcludeStrategy {
@@ -17,20 +17,26 @@ export class ExcludeIdenticalStrategy extends ExcludeStrategy {
             const current = eventLogs[i];
             const next = eventLogs[i + 1];
 
+            const {event, remarks, guardInitials} = next;
+
+            const cmpString = `${guardInitials}-${event}-${remarks}`
+
             if (current.taskEvent === TaskEvent.ARRIVE) {
                 cmpArr = [];
             }
 
-            if (next.taskEvent === TaskEvent.OTHER) {
-                const {event, remarks, guard} = next;
+            const firstAndReportEntry = i === 0 && current.taskEvent === TaskEvent.OTHER;
+            const nextIsReportEntry = next.taskEvent === TaskEvent.OTHER;
 
-                const cmpString = `${guard}-${event}-${remarks}`
-
+            if (firstAndReportEntry || nextIsReportEntry) {
                 if (_.includes(cmpArr, cmpString)) {
                     next.setExcludeReason('Gentagelse')
+                } else {
+                    cmpArr.push(cmpString);
                 }
             }
         }
+
         
         return eventLogs;
     }
